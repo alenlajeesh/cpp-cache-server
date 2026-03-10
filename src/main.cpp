c2041c2ff6b858884a7fb3995bf2ef057ea5a2c1
alenlajeesh <alenlajeesh@gmail.com>
2026-03-10 11:26:22 +0530

    release: finalize MiniRedis v1.0.0 with SAVE and QUIT commands

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 #include "../include/config.h"
 #include <iostream>
 int main()
-{
+{	
+	std::cout << "cache-server v1.0.0 starting...\n";
 	load_config("cache-server.conf");
 	std::cout << "Starting cache-server on port " << config.port << std::endl;
     load_db();
